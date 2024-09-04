e7a51fa9635d197b0c9f0ad05c10b7885b98f845
Ilana Nascimento - ARTDR <artedasredes@gmail.com>
2024-09-03 22:45:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,18 +4,34 @@ let container = document.querySelector('.container');
 let itens = container.querySelectorAll('.list .item');
 let indicador = document.querySelector('.indicators');
 let dots = indicador.querySelectorAll('ul li'); 
+let list = container.querySelector('.list')
 
 let active = 0;
 let firstPosition = 0;
 let lastPosition = itens.length - 1; 
 
-prevButton.onclick = () => {
-    
+function setSlider(){
+    let itemOld = container.querySelector('.list .item.active')
+    itemOld.classList.remove('active')
+
+    let dotsOld = indicador.querySelector('ul li.active')
+    dotsOld.classList.remove('active')
+    dots[active].classList.add('active')
+
+    indicador.querySelector('.number').innerHTML = '0' + (active + 1)
 }
 
 nextButton.onclick = () => {
-    let itemOld = container.querySelector('.list, .item.active')
-    itemOld.classlist.remove('active')
+    list.style.setProperty('--calculation', 1)
     active = active + 1 > lastPosition ? 0 : active + 1
-    itens[active].classlist.add('active')
+    setSlider()
+    itens[active].classList.add('active')
+}
+
+prevButton.onclick = () => {
+    list.style.setProperty('--calculation', -1)
+    active = active - 1 < firstPosition ? lastPosition : active - 1
+    setSlider()
+    itens[active].classList.add('active')
+
 }
